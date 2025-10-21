3ae6b0f3e00e81062be1947f80fcec412670ad86
Kamal Y <KamalY@risk.az>
2025-10-21 17:18:07 +0400

    Added abstractions for domain

---
+++ src/ProgresiumToDo.Domain/Abstractions/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace ProgresiumToDo.Domain.Abstractions;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected init; } = Guid.CreateVersion7();
+    
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    
+    public DateTime UpdatedAt { get; protected set; } = DateTime.UtcNow;
+    
+    public DateTime? DeletedAt { get; protected set; } = null;
+    
+    public void MarkAsUpdated() => UpdatedAt = DateTime.UtcNow;
+
+    public void MarkAsDeleted() => DeletedAt = DateTime.UtcNow;
+}
---
+++ src/ProgresiumToDo.Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgresiumToDo.Domain.Abstractions;
+
+public record Error(string Code, string Message)
+{
+    public static Error None = new Error(string.Empty, string.Empty);
+    
+    public static Error NullValue = new Error("Error.NullValue", "The value cannot be null.");
+}
