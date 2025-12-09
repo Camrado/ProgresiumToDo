c5228010fdd48dd1ccb8a9c30cd37ac1e7eb22fa
Camrado <blackandwhite.b17w@gmail.com>
2025-12-09 21:21:25 +0400

    Added Swagger

--- src/ProgresiumToDo.API/Program.cs
+++ src/ProgresiumToDo.API/Program.cs
@@ -1,4 +1,5 @@
 using dotenv.net;
+using Microsoft.OpenApi.Models;
 using ProgresiumToDo.API.Extensions;
 using ProgresiumToDo.Application;
 using ProgresiumToDo.Infrastructure;
@@ -9,6 +10,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Progresium ToDo API",
+        Version = "v1",
+        Description = "API documentation for the Progresium ToDo application."
+    });
+});
+
 builder.Services.AddExceptionHandling();
 
 builder.Services.AddMemoryCache();
@@ -17,6 +29,15 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Progresium ToDo API v1");
+    });
+}
 
 app.UseHttpsRedirection();
 
