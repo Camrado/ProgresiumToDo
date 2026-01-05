341d51f3846932ba8e1436c6b257728d8dc2c10f
Kamal Y <KamalY@risk.az>
2026-01-05 15:42:44 +0400

    Added migration

---
+++ src/ProgresiumToDo.Infrastructure/Migrations/20260105114212_AddedUniqueConstraintToTag.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgresiumToDo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUniqueConstraintToTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_tags_project_id",
+                table: "tags");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_tags_project_id_name",
+                table: "tags",
+                columns: new[] { "project_id", "name" },
+                unique: true,
+                filter: "\"deleted_at\" IS NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_tags_project_id_name",
+                table: "tags");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_tags_project_id",
+                table: "tags",
+                column: "project_id");
+        }
+    }
+}
