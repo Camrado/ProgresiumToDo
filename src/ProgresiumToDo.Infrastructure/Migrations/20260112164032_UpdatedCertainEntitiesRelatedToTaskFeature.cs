4c1c7a03be8ee069046770f7d25742ead4833346
Camrado <blackandwhite.b17w@gmail.com>
2026-01-12 20:41:33 +0400

    Added migration files

---
+++ src/ProgresiumToDo.Infrastructure/Migrations/20260112164032_UpdatedCertainEntitiesRelatedToTaskFeature.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgresiumToDo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedCertainEntitiesRelatedToTaskFeature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_subscriptions_user_id",
+                table: "subscriptions");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_subscriptions_user_id",
+                table: "subscriptions",
+                column: "user_id",
+                unique: true,
+                filter: "\"status\" = 'Active'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_subscriptions_user_id",
+                table: "subscriptions");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_subscriptions_user_id",
+                table: "subscriptions",
+                column: "user_id");
+        }
+    }
+}
